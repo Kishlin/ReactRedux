c2bb8fbc06e6d67ce2e051b51792af5e7c641389
Kishlin <legrand.pierrelouis@gmail.com>
2017-07-03 17:27:41 +0200

    Books reducer.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return [
+		{title: 'Who stole my Rubik\'s cube ?'},
+		{title: 'The Dark Tower'},
+		{title: 'Star Wars'},
+		{title: 'Harry Potter'},
+		{title: 'The Lord Of The Rings'}
+	]
+}
